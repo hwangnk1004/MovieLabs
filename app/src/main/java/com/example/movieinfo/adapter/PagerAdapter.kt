de6188a6485e7c105b473913c0ca03f7126de566
hwang <hwangi_@naver.com>
2022-03-05 11:31:25 +0900

    style: Clean up all log

--- app/src/main/java/com/example/movieinfo/adapter/PagerAdapter.kt
+++ app/src/main/java/com/example/movieinfo/adapter/PagerAdapter.kt
@@ -1,6 +1,5 @@
 package com.example.movieinfo.adapter
 
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
@@ -10,18 +9,9 @@ class PagerAdapter(
     private val fragments: List<Fragment>
 ) : FragmentStateAdapter(activity) {
 
-//    override fun getItemCount(): Int = fragments.size
-//
-//    override fun createFragment(position: Int): Fragment = fragments[position]
+    override fun getItemCount(): Int = fragments.size
 
-    override fun getItemCount(): Int {
-        Log.d("nkh", "getItemCount : ${fragments.size}")
-        return fragments.size
-    }
+    override fun createFragment(position: Int): Fragment = fragments[position]
 
-    override fun createFragment(position: Int): Fragment {
-        Log.d("nkh", "createFragment : ${fragments[position]}")
-        return fragments[position]
-    }
 
 }
